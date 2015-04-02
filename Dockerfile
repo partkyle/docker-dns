04745f33a043e8e6a1682a9d6e65f60c71cd04a7
Kyle Partridge <kyle.partridge@sendgrid.com>
2015-04-02 14:47:40 -0700

    support rdns lookup

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ ENV PROJECT=github.com/partkyle/docker-dns
 
 COPY . /go/src/$PROJECT
 
-RUN go install $PROJECT
+RUN go get $PROJECT && go install $PROJECT
 
 CMD docker-dns
--- main.go
+++ main.go
@@ -27,24 +27,82 @@ func (h *Handler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 	reply.SetReply(r)
 
 	answer := make([]dns.RR, 0)
+lookup:
 	for _, question := range r.Question {
-		container, err := h.docker.InspectContainer(strings.TrimSuffix(question.Name, ".docker."))
-		if err != nil {
-			log.Error(err)
-			continue
-		}
+		log.WithField("type", question.Qtype).WithField("name", question.Name).Info("resolving dns")
 
-		a := &dns.A{
-			Hdr: dns.RR_Header{
-				Name:   question.Name,
-				Rrtype: dns.TypeA,
-				Class:  dns.ClassINET,
-				Ttl:    0,
-			},
-			A: net.ParseIP(container.NetworkSettings.IPAddress),
-		}
+		switch question.Qtype {
+		case dns.TypeA:
+			container, err := h.docker.InspectContainer(strings.TrimSuffix(question.Name, ".docker."))
+			if err != nil {
+				log.Error(err)
+				continue
+			}
+			a := &dns.A{
+				Hdr: dns.RR_Header{
+					Name:   question.Name,
+					Rrtype: dns.TypeA,
+					Class:  dns.ClassINET,
+					Ttl:    0,
+				},
+				A: net.ParseIP(container.NetworkSettings.IPAddress),
+			}
 
-		answer = append(answer, a)
+			answer = append(answer, a)
+		case dns.TypeMX:
+			mx := &dns.MX{
+				Hdr: dns.RR_Header{
+					Name:   question.Name,
+					Rrtype: dns.TypeMX,
+					Class:  dns.ClassINET,
+					Ttl:    0,
+				},
+				Preference: 10,
+				Mx:         question.Name,
+			}
+
+			answer = append(answer, mx)
+		case dns.TypePTR:
+			containers, err := h.docker.ListContainers(docker.ListContainersOptions{})
+			if err != nil {
+				log.Error(err)
+				continue
+			}
+			// these look like: 73.6.17.172.in-addr.arpa.
+			reverseIP := strings.TrimSuffix(question.Name, ".in-addr.arpa.")
+			reverseParts := strings.Split(reverseIP, ".")
+
+			parts := make([]string, len(reverseParts))
+			for i := range parts {
+				parts[i] = reverseParts[len(reverseParts)-i-1]
+			}
+
+			ip := strings.Join(parts, ".")
+			log.WithField("ip", ip).Info("rDNS lookup")
+
+			for _, c := range containers {
+				container, err := h.docker.InspectContainer(c.ID)
+				if err != nil {
+					log.Error(err)
+					continue
+				}
+
+				if container.NetworkSettings.IPAddress == ip {
+					ptr := &dns.PTR{
+						Hdr: dns.RR_Header{
+							Name:   question.Name,
+							Rrtype: dns.TypePTR,
+							Class:  dns.ClassINET,
+							Ttl:    0,
+						},
+						Ptr: strings.TrimPrefix(container.Name, "/") + ".docker.",
+					}
+					answer = append(answer, ptr)
+
+					continue lookup
+				}
+			}
+		}
 	}
 
 	reply.Answer = answer
@@ -61,10 +119,20 @@ func main() {
 	log.SetOutput(os.Stdout)
 	log.SetLevel(log.DebugLevel)
 
-	certPath := os.Getenv("DOCKER_CERT_PATH")
-	client, err := docker.NewTLSClient(os.Getenv("DOCKER_HOST"), path.Join(certPath, "cert.pem"), path.Join(certPath, "key.pem"), path.Join(certPath, "ca.pem"))
-	if err != nil {
-		log.Fatal(err)
+	var client *docker.Client
+	if os.Getenv("DOCKER_HOST") != "" {
+		var err error
+		certPath := os.Getenv("DOCKER_CERT_PATH")
+		client, err = docker.NewTLSClient(os.Getenv("DOCKER_HOST"), path.Join(certPath, "cert.pem"), path.Join(certPath, "key.pem"), path.Join(certPath, "ca.pem"))
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		var err error
+		client, err = docker.NewClient("unix:///var/run/docker.sock")
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	handler := Handler{docker: client}
@@ -75,7 +143,8 @@ func main() {
 
 	dns.Handle(".", &handler)
 
-	if err := server.ListenAndServe(); err != nil {
+	err := server.ListenAndServe()
+	if err != nil {
 		log.Fatal(err)
 	}
 }
